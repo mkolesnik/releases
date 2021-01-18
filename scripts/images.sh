492addc9da913bd1c15074ef195a117a829a18f8
Mike Kolesnik <mkolesni@redhat.com>
2021-01-18 13:55:55 +0200

    Add `branch` state for creating stable branches
    This state will be used to create the initial stable branch which will
    branch off of the main branch.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/images.sh
+++ scripts/images.sh
@@ -17,6 +17,7 @@ function _pull_image() {
 function pull_images() {
     for project in ${PROJECTS[*]}; do
         clone_repo
+        checkout_project_branch
         local project_version=$(_git describe --tags --exclude="${CUTTING_EDGE}" --exclude="latest")
 
         for image in ${project_images[${project}]}; do
