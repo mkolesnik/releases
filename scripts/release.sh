e8cca16dbf7d85df067383d4ad405a03cdf69bda
Mike Kolesnik <mkolesni@redhat.com>
2021-01-21 15:22:32 +0200

    Have shipyard base image uploaded on branching
    To prevent failures due to the image not being available yet, build it
    during the branching stage and upload it.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -105,6 +105,20 @@ function adjust_shipyard() {
     sed -e "s/devel/${branch}/" -i projects/shipyard/Makefile.versions
     update_shipyard_base
     _git commit -a -s -m "Update Shipyard to use stable branch '${branch}'"
+
+    # Run in subshell so we don't change the working directory even on failure
+    (
+        set -e
+
+        # Trick our own Makefile to think we're running outside dapper
+        export DAPPER_HOST_ARCH=""
+
+        # Build and upload shipyard base image so that other projects have it immediately
+        # Otherwise, image building jobs are likely to fail when branching
+        cd projects/shipyard
+        make images
+        make release shipyard-dapper-base --tag="${release['branch']}"
+    )
 }
 
 function create_branches() {
