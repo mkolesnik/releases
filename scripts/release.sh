23699eaaaf2f07a2ddfe36ef357236a237e81ee6
Mike Kolesnik <mkolesni@redhat.com>
2021-06-08 10:58:26 +0300

    Added GH PR creation to `make release`
    The process will now automatically create a pull request to GitHub with
    the current stage data.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,14 +3,17 @@
 ## Process command line flags ##
 
 source ${SCRIPTS_DIR}/lib/shflags
+DEFINE_boolean 'dryrun' false "Set to 'true' to run without affecting any online repositories"
 DEFINE_string 'version' '' "The version to release (semver compliant)"
 DEFINE_string 'release_notes' '' "Release notes to include"
 
 FLAGS "$@" || exit $?
 eval set -- "${FLAGS_ARGV}"
 
+[[ "${FLAGS_dryrun}" = "${FLAGS_TRUE}" ]] && dryrun=true || dryrun=false
 version="${FLAGS_version}"
 release_notes="${FLAGS_release_notes}"
+declare -A next_status=( [branch]=shipyard [shipyard]=admiral [admiral]=projects [projects]=released )
 
 set -e
 
@@ -20,8 +23,18 @@ source ${SCRIPTS_DIR}/lib/debug_functions
 
 ### Functions: General ###
 
+function expect_env() {
+    local env_var="$1"
+    if [[ -z "${!env_var}" ]]; then
+        printerr "Expected environment variable ${env_var@Q} is not set"
+        exit 1
+    fi
+}
+
 function validate() {
     is_semver "$version"
+    expect_env "GITHUB_ACTOR"
+    expect_env "GITHUB_TOKEN"
 }
 
 function write() {
@@ -48,6 +61,25 @@ function write_component() {
     write "  ${project}: $(_git rev-parse HEAD)"
 }
 
+function create_pr() {
+    local branch="$1"
+    local msg="$2"
+    local base_branch="${release['branch']:-devel}"
+    local project="$(basename $(pwd))"
+    #local repo="submariner-io/${project}"
+    local repo="mkolesnik/${project}"
+
+    git add "${file}"
+    git commit -s -m "${msg}"
+    #dryrun git push -f "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${ORG}/${project}.git" "HEAD:${branch}"
+    dryrun git push -f "https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${ORG}/${project}.git" "HEAD:${branch}"
+    pr_to_review=$(dryrun gh pr create --repo "${repo}" --head "${branch}" --base "${base_branch}" --title "${msg}" --body "${msg}")
+    dryrun gh pr merge --auto --repo "${repo}" --squash "${pr_to_review}" \
+        || echo "WARN: Failed to enable auto merge on ${pr_to_review}"
+    echo "Created Pull Request: ${pr_to_review}"
+}
+
+
 function create_initial() {
     declare -gA release
     echo "Creating initial release file ${file}"
@@ -72,29 +104,26 @@ EOF
     fi
 
     # We're not branching, so just move on to shipyard
+    set_status "shipyard"
     advance_branch
 }
 
 function advance_branch() {
-    set_status "shipyard"
     write "components:"
     write_component "shipyard"
 }
 
 function advance_shipyard() {
-    set_status "admiral"
     write_component "admiral"
 }
 
 function advance_admiral() {
-    set_status "projects"
     for project in ${OPERATOR_CONSUMES[*]}; do
         write_component
     done
 }
 
 function advance_projects() {
-    set_status "released"
     write_component "submariner-operator"
     write_component "submariner-charts"
 }
@@ -105,7 +134,10 @@ function advance_stage() {
     read_release_file
     case "${release['status']}" in
     branch|shipyard|admiral|projects)
+        local next="${next_status[${release['status']}]}"
+        set_status "${next}"
         advance_${release['status']}
+        create_pr "releasing-${version}" "Advancing ${version} release to status: ${next}"
         ;;
     released)
         echo "The release ${version} has been released, nothing to do."
@@ -122,6 +154,7 @@ extract_semver "$version"
 file="releases/v${version}.yaml"
 if [[ ! -f "${file}" ]]; then
     create_initial
+    create_pr "releasing-${version}" "Initiating release of ${version}"
 else
     advance_stage
 fi
